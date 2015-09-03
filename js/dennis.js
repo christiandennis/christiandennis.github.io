2aed7d14afdf6859432e96fd626da98e6d6a1dd7
christiandennis <christiandennis11@gmail.com>
2015-09-02 23:39:39 -0700

    initial commit

---
+++ js/dennis.js
@@ -0,0 +1,46 @@
+var Dennis = (function() {
+    var start = function() {
+        // $(".content").hide();
+
+       	$(document).ready(function() {
+  			$(document).on('keyup', function() {
+  				
+			});
+
+			$(document).on('click', function() {    
+					
+			});
+
+			$(document).on('mousewheel', function() {   
+							
+			});
+
+
+		});
+
+
+    };
+
+    var updateTitle = function(){
+    	var curr = 	String($(location).attr('href'));
+				curr = curr.substring(curr.indexOf('_')+1);
+				if(curr === 'code'){
+					$('.subtitle').text('Projects');
+				} else if(curr === 'home'){
+					$('.subtitle').text('');
+				} else if(curr === 'edu'){
+					$('.subtitle').text('Education');
+				} else if(curr === 'contact'){
+					$('.subtitle').text('Contact Me');
+				} else if(curr === 'about'){
+					$('.subtitle').text('I am...');
+				};
+    }
+    
+
+
+    return {
+        start: start
+    };
+    
+})();
